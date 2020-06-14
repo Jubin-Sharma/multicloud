a7b015a9a275078e3257af9dabff418d897c6a04
Jubin-Sharma <64472963+Jubin-Sharma@users.noreply.github.com>
2020-06-14 10:18:58 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,10 @@
-Welcome to Jubin's cloud practice page.
+Welcome to Jubin's cloud practice page. 
+
+Images also uploaded in repo.
 <br/>
 <pre>
 <?php
 
-print 'ipconfig';
-
-print 'aya kuch samjh me?';
 
 ?>
 </pre>
